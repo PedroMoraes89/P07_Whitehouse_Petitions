35f642424e8b1f8f03f036de693612a7801e7d14
PEDRO HENRIQUE DE OLIVEIRA MORAES <86898701+PedroMoraes89@users.noreply.github.com>
2022-11-03 07:57:23 -0700

    Initial Commit

---
+++ P07 - Whitehouse Petitions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  P07 - Whitehouse Petitions
+//
+//  Created by user220265 on 11/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
